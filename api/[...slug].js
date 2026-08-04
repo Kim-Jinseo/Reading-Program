7e5869e7c553f827360cfc9c8d70594693a13b68
Kim Jinseo <joshep3407@naver.com>
2026-08-04 18:56:44 +0800

    Test vanilla Node function

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,6 +1,1 @@
-import { Hono } from 'hono';
-import { handle } from 'hono/vercel';
-const app = new Hono().basePath('/api');
-app.get('/health', (c) => c.json({ status: 'ok', msg: 'Node.js Serverless works' }));
-app.post('/auth/login', (c) => c.json({ status: 'ok', msg: 'POST auth login Node works' }));
-export default handle(app);
+export default function handler(req, res) { res.status(200).json({ status: 'ok', msg: 'Vanilla Node works' }); }
