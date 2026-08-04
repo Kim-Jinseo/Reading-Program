6d9eeb031ebf3b3def1c6cf19469864722f97184
Kim Jinseo <joshep3407@naver.com>
2026-08-04 18:57:16 +0800

    Test Web Standard native routing on Vercel

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,1 +1,4 @@
-export default function handler(req, res) { res.status(200).json({ status: 'ok', msg: 'Vanilla Node works' }); }
+import { Hono } from 'hono';
+const app = new Hono().basePath('/api');
+app.get('/health', (c) => c.json({ status: 'ok', msg: 'Web Standard works on Node' }));
+export default function(req) { return app.fetch(req); }
